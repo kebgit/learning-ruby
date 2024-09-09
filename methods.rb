b6fbab08c18fae9a6a52084a4e184e99ccd4da60
cubercubegit <102414081+cubercubegit@users.noreply.github.com>
2024-09-09 08:43:33 +0100

    Practiced methods

---
+++ methods.rb
@@ -0,0 +1,38 @@
+# def say(words)
+#   puts words + '.'    ## <= We only make the change here!
+# end
+
+# say("hello")
+# say("hi")
+# say("how are you")
+# say("I'm fine")
+# 
+# def greeting(name)
+#   puts "Hello #{name}"
+# end
+
+# puts"What is your name?"
+# greeting(gets)
+# 
+#---
+#
+# def multiply(a,b)
+#   puts a*b
+#   return a*b
+# end
+
+# puts "Number 1:"
+# first = gets.chomp.to_i
+# puts "Number 2:"
+# second = gets.chomp.to_i
+
+# puts "Product"
+# multiply(first,second)
+# ---
+#
+# def scream(words)
+#   words = words + "!!!!"
+#   puts words
+# end
+
+# scream("Yippeee")
